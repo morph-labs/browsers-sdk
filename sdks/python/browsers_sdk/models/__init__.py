005147014828fe0c3ed49b4e93369ba3935f32f1
abetlen <6826477+abetlen@users.noreply.github.com>
2025-10-05 08:29:22 +0000

    chore(sdk): regenerate after removing sign-up/login

--- sdks/python/browsers_sdk/models/__init__.py
+++ sdks/python/browsers_sdk/models/__init__.py
@@ -1,14 +1,12 @@
 """Contains all the data models used in inputs/outputs"""
 
 from .http_validation_error import HTTPValidationError
-from .message import Message
 from .session import Session
 from .session_list import SessionList
 from .validation_error import ValidationError
 
 __all__ = (
     "HTTPValidationError",
-    "Message",
     "Session",
     "SessionList",
     "ValidationError",
--- sdks/typescript/index.ts
+++ sdks/typescript/index.ts
@@ -11,7 +11,6 @@ export { OpenAPI } from './core/OpenAPI';
 export type { OpenAPIConfig } from './core/OpenAPI';
 
 export type { HTTPValidationError } from './models/HTTPValidationError';
-export type { Message } from './models/Message';
 export type { Session } from './models/Session';
 export type { SessionList } from './models/SessionList';
 export type { ValidationError } from './models/ValidationError';
